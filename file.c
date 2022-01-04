b2f58368c2cc3bc6b6132606479164d1677af1b3
LaurGen <oprealaur@yahoo.com>
2022-01-04 03:38:52 +0200

    cu research la atomice

--- file.c
+++ file.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdatomic.h>
+
 // __sync_fetch_and_add(adresa, valoare)   --adunare atomica din stdlib
 // atomic_compare_exchange_weak(obiect_volatil, expected, dorit)    --compare and swap din stdatomic.h
 // atomic_exchange(obiect_volatil, dorit)   --atribuire din stdatomic.h
+// struct atomic_flag nume;     --FLAG lock-free atomic; nu accesez sau atribui valori ci folosesc:
+// bool cond = atomic_flag_test_and_set(volatile atomic_flag* obj)    --seteaza flagu TRUE si intoarce valoarea precedenta
+// void atomic_flag_clear(volatile atomic_flag* obj)     --seteaza flagu FALSE
+
 
 // Bune doar daca folosesc tipuri de date atomice:
 // int x = atomic_load(obiect_volatil) --ia valoarea dintr-o variabila atomica
