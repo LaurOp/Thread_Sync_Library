ec90864209362b0fc1654b557288ce160b686e4f
RookerStyle <45435659+RookerStyle@users.noreply.github.com>
2022-01-08 02:15:54 +0200

    mutex + rwlocks

--- sinc.c
+++ sinc.c
@@ -28,3 +28,59 @@ int unlock(Mtx* Mut) {
 
 void mtx_destroy(Mtx* Mut){
 }
+
+
+
+void sem_binar_init(Sem_binar* sb){
+    atomic_flag_clear(&sb->flg);
+}
+
+void lock_sb(Sem_binar* sb){
+    while (atomic_flag_test_and_set(&sb->flg));
+}
+
+int unlock_sb(Sem_binar* sb){
+    atomic_flag_clear(&sb->flg);
+}
+
+void sem_binar_destroy(Sem_binar* sb){
+    atomic_flag_clear(&sb->flg);
+}
+
+
+
+void rwl_init(Rwlock* rwl){
+    rwl->counter = 0;
+    sem_binar_init(&rwl->counter_lock);
+    mtx_init(&rwl->write_lock);
+}
+
+void lock_for_reading(Rwlock* rwl)
+{   
+    lock_sb(&rwl->counter_lock);
+    rwl->counter++;
+    if(rwl->counter == 1)
+        lock(&rwl->write_lock);
+    unlock_sb(&rwl->counter_lock);
+}
+
+void unlock_for_reading(Rwlock* rwl)
+{   
+    lock_sb(&rwl->counter_lock);
+    rwl->counter--;
+    if(rwl->counter == 0)
+        unlock(&rwl->write_lock);
+    unlock_sb(&rwl->counter_lock);
+}
+
+void lock_for_writing(Rwlock* rwl)
+{
+    lock(&rwl->write_lock);
+}
+
+void unlock_for_writing(Rwlock* rwl)
+{   
+    unlock(&rwl->write_lock);
+    sem_binar_destroy(&rwl->counter_lock);
+    mtx_destroy(&rwl->write_lock);
+}
