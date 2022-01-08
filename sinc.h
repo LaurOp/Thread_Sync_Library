47b7ab332e52d45698de1b7f9823d07b2c21acde
RookerStyle <45435659+RookerStyle@users.noreply.github.com>
2022-01-08 02:16:16 +0200

    headerul librariei

--- sinc.h
+++ sinc.h
@@ -1,5 +1,6 @@
 #ifndef SINC_H_
 #define SINC_H_
+#include <stdatomic.h>
 
 typedef struct{
     volatile _Atomic int owner;
@@ -10,5 +11,27 @@ void lock(Mtx* Mut);
 int unlock(Mtx* Mut);
 void mtx_destroy(Mtx* Mut);
 
+typedef struct{
+    volatile atomic_flag flg;
+} Sem_binar;        //UN MUTEX CARE NU VERIFICA OWNERSHIP
+
+void sem_binar_init(Sem_binar* sb);
+void lock_sb(Sem_binar* sb);
+int unlock_sb(Sem_binar* sb);
+void sem_binar_destroy(Sem_binar* sb);
+
+
+typedef struct{
+    Mtx write_lock;
+    Sem_binar counter_lock;
+    int counter;
+} Rwlock;
+
+void rwl_init(Rwlock* rwl);
+void lock_for_reading(Rwlock* rwl);
+void unlock_for_reading(Rwlock* rwl);
+void lock_for_writing(Rwlock* rwl);
+void unlock_for_writing(Rwlock* rwl);
+void rwl_destroy(Rwlock* rwl);
 
 #endif
