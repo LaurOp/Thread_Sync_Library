7e0a8b4f09d5533d8bb18eeca8622a734317c160
RookerStyle <45435659+RookerStyle@users.noreply.github.com>
2022-01-09 18:46:09 +0200

    test semafor 3

--- test3_sem.c
+++ test3_sem.c
@@ -2,60 +2,42 @@
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "sinc.h"
 
-#define NITER 1000000
+#define nMAX 315153
 
-int cnt = 0;
-sem_t mutex;
+Semafor s;
+int nr = 0;
 
-void * Count(void * a)
-{
-    int i, tmp;
-    for(i = 0; i < NITER; i++)
-    {
-        sem_wait(&mutex);
-        tmp = cnt;      /* copy the global cnt locally */
-        tmp = tmp+1;    /* increment the local copy */
-        cnt = tmp;      /* store the local value into the global cnt */ 
-        sem_post(&mutex);
+void* fct(void * a){
+    int i,aux;
+    //int* q = malloc(sizeof(int));
+    for(i = 0; i < nMAX; i++){
+        s_wait(&s);
+        // s_getvalue(q, &s);
+        // printf("%d\n", *q);
+        aux = nr; 
+        aux = aux+1;
+        nr = aux; 
+        s_post(&s);
     }
 }
 
-int main(int argc, char * argv[])
-{
-    pthread_t tid1, tid2;
-    sem_init(&mutex, 0, 1);
+int main(){    
+    _s_init(&s, 1);
 
-    if(pthread_create(&tid1, NULL, Count, NULL))
-    {
-      printf("\n ERROR creating thread 1");
-      exit(1);
-    }
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, fct, NULL);
+    pthread_create(&t2, NULL, fct, NULL);
 
-    if(pthread_create(&tid2, NULL, Count, NULL))
-    {
-      printf("\n ERROR creating thread 2");
-      exit(1);
-    }
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
 
-    if(pthread_join(tid1, NULL))	/* wait for the thread 1 to finish */
-    {
-      printf("\n ERROR joining thread");
-      exit(1);
-    }
-
-    if(pthread_join(tid2, NULL))        /* wait for the thread 2 to finish */
-    {
-      printf("\n ERROR joining thread");
-      exit(1);
-    }
-
-    if (cnt < 2 * NITER) 
-        printf("\n BOOM! cnt is [%d], should be %d\n", cnt, 2*NITER);
+    if (nr < 2 * nMAX) 
+        printf("\n FAIL! nr = %d, trebuia = %d\n", nr, 2*nMAX);
     else
-        printf("\n OK! cnt is [%d]\n", cnt);
-  
-    pthread_exit(NULL);
-    sem_destroy(&mutex);
+        printf("\n OK! nr = [%d]\n", nr);
+
+    s_destroy(&s);
 }
 
